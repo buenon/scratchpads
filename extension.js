bae8cf989b836cea7edde2a66bcda10a9327022c
Nadav Bueno <buenonadav@gmail.com>
2018-05-31 00:22:12 +0300

    Basic functionality

--- extension.js
+++ extension.js
@@ -1,30 +1,56 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+const fs = require('fs');
+const path = require('path');
 const vscode = require('vscode');
+
+exports.activate = activate;
+exports.deactivate = deactivate;
+
+"use strict";
+const fileName = "scratch";
+let basePath;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
+    init(context);
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "scratchpads" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
+    let disposable = vscode.commands.registerCommand('extension.openTypedScratch', createScratch);
     context.subscriptions.push(disposable);
 }
-exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {
+function init(context) {
+    basePath = context.extensionPath;
 }
-exports.deactivate = deactivate;
+
+function deactivate() {
+    removeScratches();
+}
+
+function createScratch() {
+    let i = undefined;
+    let initialFilename = path.join(basePath, fileName);
+    let fullPath = initialFilename;
+
+    while (fs.existsSync(fullPath)) {
+        i = i ? i + 1 : 1;
+        fullPath = initialFilename + i;
+    }
+
+    fs.writeFileSync(fullPath, "");
+
+    vscode.workspace.openTextDocument(fullPath).then(doc => {
+        vscode.window.showTextDocument(doc);
+    });
+}
+
+function removeScratches() {
+    fs.readdir(basePath, (err, files) => {
+        for (var i = 0, len = files.length; i < len; i++) {
+            var match = files[i].match(fileName + ".*");
+            if (match !== null)
+                fs.unlink(path.join(basePath, match[0]));
+        }
+    });
+}
