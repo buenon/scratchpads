efbf5b315f6493ad30608021ce03de9765cf22a4
Nadav bueno <buenonadav@gmail.com>
2025-09-23 21:27:09 +0300

    #62: Display scratchpads in file explorer (#65)

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,8 @@
 import * as vscode from 'vscode';
-import {Config} from './config';
-import {FiletypesManager} from './filetypes.manager';
-import {ScratchpadsManager} from './scratchpads.manager';
+import { Config } from './config';
+import { FiletypesManager } from './filetypes.manager';
+import { ScratchpadTreeProvider } from './scratchpad-tree-provider';
+import { ScratchpadsManager } from './scratchpads.manager';
 import Utils from './utils';
 
 /**
@@ -16,6 +17,15 @@ export function activate(context: vscode.ExtensionContext) {
 
   const scratchpadsManager = new ScratchpadsManager(new FiletypesManager());
 
+  // Register tree view
+  const treeViewProvider = new ScratchpadTreeProvider();
+  const treeView = vscode.window.createTreeView('scratchpads', {
+    treeDataProvider: treeViewProvider,
+    showCollapseAll: false,
+  });
+  context.subscriptions.push(treeView);
+  context.subscriptions.push(treeViewProvider);
+
   const commands: { [key: string]: (...args: any[]) => any } = {
     'scratchpads.newScratchpad': () => Utils.confirmFolder() && scratchpadsManager.createScratchpad(),
     'scratchpads.newScratchpadDefault': () => Utils.confirmFolder() && scratchpadsManager.createScratchpadDefault(),
@@ -25,6 +35,11 @@ export function activate(context: vscode.ExtensionContext) {
     'scratchpads.removeAllScratchpads': () => Utils.confirmFolder() && scratchpadsManager.removeAllScratchpads(),
     'scratchpads.removeScratchpad': () => Utils.confirmFolder() && scratchpadsManager.removeScratchpad(),
     'scratchpads.newFiletype': () => Utils.confirmFolder() && scratchpadsManager.newFiletype(),
+    'scratchpads.tree.rename': (fileName: string) => Utils.confirmFolder() && treeViewProvider.renameFile(fileName),
+    'scratchpads.tree.delete': (fileName: string) => Utils.confirmFolder() && treeViewProvider.deleteFile(fileName),
+    'scratchpads.tree.new': () => Utils.confirmFolder() && scratchpadsManager.createScratchpad(),
+    'scratchpads.tree.newDefault': () => Utils.confirmFolder() && scratchpadsManager.createScratchpadDefault(),
+    'scratchpads.tree.removeAll': () => Utils.confirmFolder() && scratchpadsManager.removeAllScratchpads(),
   };
 
   for (const command in commands) {
